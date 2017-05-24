36b60dc9aa84df8ba4c56f2d86ff46e0f095a597
Patrick Crosby <pcrosby@gmail.com>
2017-05-24 11:02:22 -0500

    Add AddWriter

---
+++ go/teams/member.go
@@ -0,0 +1,11 @@
+package teams
+
+import (
+	"golang.org/x/net/context"
+
+	"github.com/keybase/client/go/libkb"
+)
+
+func AddWriter(ctx context.Context, g *libkb.GlobalContext, teamname, username string) error {
+	return nil
+}
