e7ff0b9db0a660728bf373681bd5e5e3d77b9e73
Patrick Crosby <pcrosby@gmail.com>
2017-05-24 10:37:24 -0500

    Fix DeepCopy of EldestSeqno

--- go/protocol/keybase1/extras.go
+++ go/protocol/keybase1/extras.go
@@ -945,6 +945,7 @@ func (u *UserPlusKeys) DeepCopy() UserPlusKeys {
 		Uvv:               u.Uvv,
 		DeletedDeviceKeys: append([]PublicKey{}, u.DeletedDeviceKeys...),
 		PerUserKeys:       append([]PerUserKey{}, u.PerUserKeys...),
+		EldestSeqno:       u.EldestSeqno,
 	}
 }
 
